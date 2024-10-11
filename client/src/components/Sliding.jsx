230ee2a4af7e1c29508fddc633e7be0c4d55a68f
shubham <shubhampatil5333@gmail.com>
2024-10-11 19:24:19 +0530

    changes done

--- client/src/components/Sliding.jsx
+++ client/src/components/Sliding.jsx
@@ -26,10 +26,28 @@ const Sliding = ({ slides }) => {
               alt={`Slide ${index + 1}`}
             />
             {index === 0 && (
-              <div className='absolute inset-0 flex flex-col text-white'>
-                <h2 className='text-4xl mt-60 px-9 py-3 font-nsans-bold '>Welcome to Our Slideshow</h2>
-                <button className=' bg-blue-600 px-2  py-3'>
-                  Learn More
+              <div className='absolute inset-0 flex flex-col items-center justify-center text-white'>
+                <h1 className='text-4xl text-green-400 font-bold font-nsans-bold mb-4'>HEY!</h1>
+                <h3 className='text-3xl text-white font-bold font-nsans-bold mb-4'>WELCOME TO EZ CHARGE</h3><br />
+                <button className='px-6 py-3 font-bold font-nsans-bold bg-green-500 rounded-lg flex cursor-pointer items-center'>
+                  Sign In <FaCircleArrowRight className='ml-2' />
+                </button>
+              </div>
+            )}
+
+            {index === 1 && (
+              <div className='absolute inset-0 flex flex-col items-center justify-center text-white'>
+                 <h3 className='text-3xl text-white font-bold font-nsans-bold mb-4'>ADD CHARGING STATION</h3><br />
+                 <button className='px-6 py-3 font-bold font-nsans-bold text-black bg-white rounded-lg flex cursor-pointer items-center'>
+                  Add now <FaCircleArrowRight className='ml-2' />
+                </button>
+              </div>
+            )}
+            {index === 2 && (
+              <div className='absolute inset-0 flex flex-col items-center justify-center text-white'>
+                <h3 className='text-3xl text-white font-bold font-nsans-bold mb-4'>FIND EV CHARGING STATION</h3><br />
+                 <button className='px-6 py-3 font-bold font-nsans-bold text-black bg-white rounded-lg flex cursor-pointer items-center'>
+                  FIND NOW <FaCircleArrowRight className='ml-2' />
                 </button>
               </div>
             )}
@@ -52,9 +70,8 @@ const Sliding = ({ slides }) => {
             <div
               onClick={() => setCurrent(i)}
               key={'circle' + i}
-              className={`rounded-full w-5 h-5 cursor-pointer ${
-                i === current ? 'bg-white' : 'bg-gray-400'
-              }`}
+              className={`rounded-full w-5 h-5 cursor-pointer ${i === current ? 'bg-white' : 'bg-gray-400'
+                }`}
             ></div>
           );
         })}
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import banner1 from '../assets/banner2black.png';
-// import banner2 from '../assets/addStation.png';
+import banner1 from '../assets/Fblack.png';
+import banner2 from '../assets/bedit.png';
+import banner3 from '../assets/bm.jpg';
 
 import Sliding from '../components/Sliding';
 
 const Home = () => {
-  let slides = [banner1, banner1, banner1]; 
+  let slides = [banner1, banner2, banner3]; 
   return (
     <div className=' min-h-screen mt-9 mb-9 w-[80%] m-auto'>
       <Sliding slides={slides} />
